d1cec0c23b435f4a8684d1219b95471cac2df211
Luo <luos@C02N64X2G3QP.local>
2022-07-18 17:08:29 -0500

    Add buttons to source codes (edit, mark done/not done), check time conflicts in newEvent, sort events;  version 2 review is added in doc folder

--- week_todo_app.py
+++ week_todo_app.py
@@ -6,6 +6,8 @@
 
 from tkinter import *
 from tkinter import messagebox
+from tktimepicker import SpinTimePickerModern, constants # pip install tktimepicker
+
 
 print('Launch App ====== ')
 
@@ -13,32 +15,17 @@ print('Launch App ====== ')
 
 # --------- event functions ---------- #
 
-def displayEvent(text,todo_this_week,whichday):
+def displayEvent(text, todo_this_week, day_dict, day_button):
     """
     display events of selected day
     INPUT:
     text: indicate which day of the week
     todo_this_week: the dictionary contains every weekday's todo list
-    whichday: a one-item list to keep track of which weekday, whichday will be updated to the selected day (text) once the button of this day is clicked. it will be used later for editing events
+    day_dict: a one-item list to keep track of which weekday, day_dict will be updated to the selected day (text) once the button of this day is clicked. it will be used later for editing events
     """
 
     # determine which day (given by text) to work on, pull out this day's todo list
-    if text == "MON":
-      event_list = todo_this_week['MON']
-    elif text == "TUE":
-      event_list = todo_this_week['TUE']
-    elif text == "WED":
-      event_list = todo_this_week['WED']
-    elif text == "THU":
-      event_list = todo_this_week['THU']
-    elif text == "FRI":
-      event_list = todo_this_week['FRI']
-    elif text == "SAT":
-      event_list = todo_this_week['SAT']
-    elif text == "SUN":
-      event_list = todo_this_week['SUN']
-    else:
-      event_list = []
+    event_list = todo_this_week[text]
 
     #clear listbox and show this day's to do list
     lb.delete(0,END)
@@ -52,48 +39,218 @@ def displayEvent(text,todo_this_week,whichday):
        item_details = item["name"] + " from " + item["from"] + " to " + item["to"] + iscomplete
        lb.insert(END, item_details)
     
-    #update whichday to the selected day, which is to be used for add and remove event
-    whichday.pop(0)
-    whichday.append(text)
+    #update day_dict to the selected day, which is to be used for add and remove event
+    day_dict["current"] = text
+
+    # set the last active button to inactive
+    last_day = day_dict["last"]
+    day_button[last_day].configure(highlightbackground="white")
+
+    # Change color to show which day is currently active
+    curr_day = day_dict["current"]
+    day_button[curr_day].configure(highlightbackground="yellow")
+
+    # update last day so the now current day will be set inactive
+    # next time
+    day_dict["last"] = curr_day
    
 #add event to select weekday
-def newEvent(whichday,todo_this_week):
+def newEvent(day_dict,todo_this_week):
+
+    #first check the input time period is legit
+    fromtime = from_entry.hours() + from_entry.minutes()/60.0;
+    totime = to_entry.hours() + to_entry.minutes()/60.0;
+    if fromtime>=totime:
+       messagebox.showwarning("warning", "Please enter legit time period.")
+       return
+       
+    #check if input time has conflict with existing events of this day
+    thisday = day_dict["current"]
+    thisday_events =todo_this_week[thisday]
+    len_events = len(thisday_events)
+    for item in range(len_events):
+        this_event = thisday_events[item]
+        if this_event["done"] == False:
+           str1 = this_event["from"]
+           str2 = this_event["to"]
+        
+           str1list = str1.split(":")
+           str2list = str2.split(":")
+           this_event_fromtime = float(str1list[0]) + float(str1list[1])/60.0;
+           this_event_totime = float(str2list[0]) + float(str2list[1])/60.0;
+        
+           if fromtime>=this_event_fromtime and fromtime<=this_event_totime:
+              messagebox.showwarning("warning", "Time conflict: bad starting time.")
+              return
+           
+           if totime>=this_event_fromtime and totime<=this_event_totime:
+              messagebox.showwarning("warning", "Time conflict: bad ending time.")
+              return
+        
+       
     #read event from entries
     event = event_entry.get()
-    fromtime = from_entry.get()
-    totime = to_entry.get()
+
+    # need to composite hours and minutes from time picker
+    # you could improve this be always having 2 digit minutes (i.e. 6:00 instead of 6:0)
+    fromtime = str(from_entry.hours()) + ":" + str(from_entry.minutes())
+    totime = str(to_entry.hours()) + ":" + str(to_entry.minutes())
     isdone = False
     if isdone == False:
        iscomplete = " "
     else:
-       iscomplete = "DONE!"
+       iscomplete = " DONE!"
     task = event + " from " + fromtime + " to " + totime + " " + iscomplete
     
     #update listbox (added to end) for this day
     if event != "":
         lb.insert(END, task)
         event_entry.delete(0, "end")
-        from_entry.delete(0, "end")
-        to_entry.delete(0, "end")
+        
+        # I don't think there's a need to reset the times
+        #from_entry.delete(0, "end")
+        #to_entry.delete(0, "end")
+
         #update todo list for this day in todo_this_week (append to end)
-        thisday = whichday[0]
+        thisday = day_dict["current"]
         ev = {"name":event, "from": fromtime, "to": totime, "done":isdone}
         todo_this_week[thisday].append(ev)
+        
+        #sort today's events by from time and re_display them
+        todo_this_week[thisday] = sorted(todo_this_week[thisday], key = getFromtime)
+        
+        #clear listbox and show this day's to do list
+        lb.delete(0,END)
+        #show events for selected day
+        for item in todo_this_week[thisday]:
+            iscomplete = " "
+            if item["done"] == False:
+               iscomplete = " "
+            else:
+               iscomplete = " DONE!"
+            item_details = item["name"] + " from " + item["from"] + " to " + item["to"] + iscomplete
+            lb.insert(END, item_details)
+        
     else:
         messagebox.showwarning("warning", "Please enter some event.")
-     
 
+def getFromtime(this_event):
+        str1 = this_event["from"]
+        str2 = this_event["to"]
+        
+        str1list = str1.split(":")
+        str2list = str2.split(":")
+        this_event_fromtime = float(str1list[0]) + float(str1list[1])/60.0;
+        this_event_totime = float(str2list[0]) + float(str2list[1])/60.0;
+        
+        return this_event_fromtime
+        
 #remove event from select weekday
-def deleteEvent(whichday,todo_this_week):
+def deleteEvent(day_dict,todo_this_week):
     #delete event from listbox and from todo_this_week
-    thisday = whichday[0]
+    thisday = day_dict["current"]
     for item in lb.curselection():
         lb.delete(item)
-        todo_this_week[thisday].pop(item)
+
+        #todo_this_week[thisday].pop(item)
+        del todo_this_week[thisday][item] # I think this better shows that the item go deleted (del)
   
 
+
+#edit select event from select weekday
+def editEvent(day_dict,todo_this_week):
+    #edit event from listbox and from todo_this_week
+    thisday = day_dict["current"]
+    
+    for item in lb.curselection():
+        #read event from entry
+        event = event_entry.get()
+        fromtime = str(from_entry.hours()) + ":" + str(from_entry.minutes())
+        totime = str(to_entry.hours()) + ":" + str(to_entry.minutes())
+        isdone = todo_this_week[thisday][item]["done"]
+        
+        if isdone == False:
+           iscomplete = " "
+        else:
+           iscomplete = " DONE!"
+        task = event + " from " + fromtime + " to " + totime + " " + iscomplete
+    
+        lb.delete(item)
+        todo_this_week[thisday].pop(item)
+         
+        #update listbox for this day
+        if task != "":
+          lb.insert(item, task)
+          event_entry.delete(0, "end")
+          #from_entry.hours.delete(0, "end")
+          #to_entry.mimutes.delete(0, "end")
+        else:
+          messagebox.showwarning("warning", "Please enter some task.")
+        #update todo list for this day in todo_this_week
+        ev = {"name":event, "from": fromtime, "to": totime, "done":isdone}
+        todo_this_week[thisday].insert(item,ev)
+        
+#mark select event from select weekday to be completed
+def markEventDone(day_dict,todo_this_week):
+    #edit event from listbox and from todo_this_week
+    thisday = day_dict["current"]
+    
+    for item in lb.curselection():
+        #read selected event
+        event = todo_this_week[thisday][item]["name"]
+        fromtime = todo_this_week[thisday][item]["from"]
+        totime = todo_this_week[thisday][item]["to"]
+        isdone = todo_this_week[thisday][item]["done"]
+        
+        #update event mark
+        isdone = True
+    
+        #update listbox
+        if isdone == False:
+           iscomplete = " "
+        else:
+           iscomplete = " DONE!"
+        task = event + " from " + fromtime + " to " + totime + " " + iscomplete
+        lb.delete(item)
+        lb.insert(item, task)
+        
+        #update todo_this_week
+        todo_this_week[thisday].pop(item)
+        ev = {"name":event, "from": fromtime, "to": totime, "done":isdone}
+        todo_this_week[thisday].insert(item,ev)
+                
+#mark select event from select weekday to be not yet completed
+def markEventNotDone(day_dict,todo_this_week):
+    #edit event from listbox and from todo_this_week
+    thisday = day_dict["current"]
+    
+    for item in lb.curselection():
+        #read selected event
+        event = todo_this_week[thisday][item]["name"]
+        fromtime = todo_this_week[thisday][item]["from"]
+        totime = todo_this_week[thisday][item]["to"]
+        isdone = todo_this_week[thisday][item]["done"]
+        
+        #update event mark
+        isdone = False
+    
+        #update listbox
+        if isdone == False:
+           iscomplete = " "
+        else:
+           iscomplete = " DONE!"
+        task = event + " from " + fromtime + " to " + totime + " " + iscomplete
+        lb.delete(item)
+        lb.insert(item, task)
+        
+        #update todo_this_week
+        todo_this_week[thisday].pop(item)
+        ev = {"name":event, "from": fromtime, "to": totime, "done":isdone}
+        todo_this_week[thisday].insert(item,ev)
+        
+        
 # --------------------------------------------- #
-# initialize disctionary for todo list of the week
+# initialize dictionary for todo list of the week
 todo_this_week = {"MON": [], "TUE": [], "WED": [], "THU": [], "FRI": [], "SAT": [], "SUN": []}
 
 
@@ -139,9 +296,13 @@ sb.config(command=lb.yview)
 button_frame = Frame(app_win)
 button_frame.pack(pady=20)
 
-whichday = ["MON"] # for indicating which day of the week
+# As we need to keep track of 2 tyes of days, I'm using a dict here instead of a list
+day_dict = {"current":"MON", "last":"MON"}
 
-addDay = Button(
+# storing the buttons in a dict here so we can change their appearance later
+day_button = {}
+
+day_button["MON"] = Button(
     button_frame,
     text='MON',
     font=('times 14'),
@@ -151,10 +312,11 @@ addDay = Button(
     activeforeground = 'green',
     padx=20,
     pady=10,
-    command= lambda: displayEvent('MON',todo_this_week,whichday)
-).grid(row=1,column=0)
+    command= lambda: displayEvent('MON',todo_this_week,day_dict, day_button)
+)
+day_button["MON"].grid(row=1,column=0)
 
-addDay = Button(
+day_button["TUE"] = Button(
     button_frame,
     text='TUE',
     font=('times 14'),
@@ -164,11 +326,12 @@ addDay = Button(
     activeforeground = 'green',
     padx=20,
     pady=10,
-    command= lambda: displayEvent('TUE',todo_this_week,whichday)
-).grid(row=1,column=1)
+    command= lambda: displayEvent('TUE',todo_this_week,day_dict, day_button)
+)
+day_button["TUE"].grid(row=1,column=1)
 
 
-addDay = Button(
+day_button["WED"] = Button(
     button_frame,
     text='WED',
     font=('times 14'),
@@ -178,11 +341,12 @@ addDay = Button(
     activeforeground = 'green',
     padx=20,
     pady=10,
-    command= lambda:  displayEvent('WED',todo_this_week,whichday)
-).grid(row=1,column=2)
+    command= lambda:  displayEvent('WED',todo_this_week,day_dict, day_button)
+)
+day_button["WED"].grid(row=1,column=2)
 
 
-addDay = Button(
+day_button["THU"] = Button(
     button_frame,
     text='THU',
     font=('times 14'),
@@ -192,10 +356,11 @@ addDay = Button(
     activeforeground = 'green',
     padx=20,
     pady=10,
-    command= lambda:  displayEvent('THU',todo_this_week,whichday)
-).grid(row=1,column=3)
+    command= lambda:  displayEvent('THU',todo_this_week,day_dict, day_button)
+)
+day_button["THU"].grid(row=1,column=3)
 
-addDay = Button(
+day_button["FRI"] = Button(
     button_frame,
     text='FRI',
     font=('times 14'),
@@ -205,11 +370,12 @@ addDay = Button(
     activeforeground = 'green',
     padx=20,
     pady=10,
-    command= lambda:   displayEvent('FRI',todo_this_week,whichday)
-).grid(row=1,column=4)
+    command= lambda:   displayEvent('FRI',todo_this_week,day_dict, day_button)
+)
+day_button["FRI"].grid(row=1,column=4)
 
 
-addDay = Button(
+day_button["SAT"] = Button(
     button_frame,
     text='SAT',
     font=('times 14'),
@@ -219,10 +385,11 @@ addDay = Button(
     activeforeground = 'green',
     padx=20,
     pady=10,
-    command= lambda:  displayEvent('SAT',todo_this_week,whichday)
-).grid(row=1,column=5)
+    command= lambda:  displayEvent('SAT',todo_this_week,day_dict, day_button)
+)
+day_button["SAT"].grid(row=1,column=5)
 
-addDay = Button(
+day_button["SUN"] = Button(
     button_frame,
     text='SUN',
     font=('times 14'),
@@ -232,8 +399,16 @@ addDay = Button(
     activeforeground = 'green',
     padx=20,
     pady=10,
-    command= lambda:  displayEvent('SUN',todo_this_week,whichday)
-).grid(row=1,column=6)
+    command= lambda:  displayEvent('SUN',todo_this_week,day_dict, day_button)
+)
+day_button["SUN"].grid(row=1,column=6)
+
+# set current day as active
+day = day_dict["current"]
+day_button[day].configure(bg="yellow")
+
+
+
 
 # --------------------------------------------- #
 
@@ -251,27 +426,54 @@ event_entry = Entry(
     width = 20
     )
 event_label.grid(row=2,column=0,sticky=W)
-event_entry.grid(row=2,column=1,sticky=W)
+event_entry.grid(row=2,column=1,columnspan=3, sticky=W)
 
 #enter event starting time
 from_label = Label(entry_frame,text = "From:")
+# https://github.com/PaulleDemon/tkTimePicker/blob/master/examples/SpinTimePickerExample.py
+from_entry = SpinTimePickerModern(entry_frame)
+from_entry.set24Hrs(6)  # initial time to show
+from_entry.setMins(00)
+from_entry.addAll(constants.HOURS24)  # adds hours clock, minutes and period
+from_entry.configureAll(height=1, width=3,
+                            font=("Times", 24),
+                            #hovercolor="red",
+                            #clickedbg="white",
+                            clickedcolor="black")
+#from_entry.configure_separator(bg="#404040", fg="#ffffff")
+'''
 from_entry = Entry(
     entry_frame,
     font=('times', 24),
     width = 20
     )
-from_label.grid(row=3,column=0,sticky=W)
-from_entry.grid(row=3,column=1,sticky=W)
+'''
+
+
+from_label.grid(row=2,column=4,sticky=W)
+from_entry.grid(row=2,column=5,sticky=W)
 
 #enter event finishing time
 to_label = Label(entry_frame,text = "To:")
+to_entry = SpinTimePickerModern(entry_frame)
+to_entry.addAll(constants.HOURS24)  # adds hours clock, minutes and period
+to_entry.set24Hrs(7)  # initial time to show
+to_entry.setMins(00)
+to_entry.configureAll(height=1, width=3,
+                            font=("Times", 24),
+                            #hovercolor="red",
+                            #clickedbg="white",
+                            clickedcolor="black")
+#to_entry.configure_separator(bg="#404040", fg="#ffffff")
+'''
 to_entry = Entry(
     entry_frame,
     font=('times', 24),
     width = 20
     )
-to_label.grid(row=4,column=0,sticky=W)
-to_entry.grid(row=4,column=1,sticky=W)
+'''
+to_label.grid(row=2,column=6,sticky=W)
+to_entry.grid(row=2,column=7,sticky=W)
 
 # --------------------------------------------- #
 
@@ -289,9 +491,9 @@ addEvent_btn = Button(
     fg='green',
     padx=20,
     pady=10,
-    command=lambda:newEvent(whichday,todo_this_week)
+    command=lambda:newEvent(day_dict,todo_this_week)
 )
-addEvent_btn.grid(row=5,column=0,columnspan=3)
+addEvent_btn.grid(row=3,column=0,columnspan=3)
 
 
 
@@ -303,9 +505,52 @@ delEvent_btn = Button(
     fg='red',
     padx=20,
     pady=10,
-    command=lambda:deleteEvent(whichday,todo_this_week)
+    command=lambda:deleteEvent(day_dict,todo_this_week)
 )
-delEvent_btn.grid(row=5,column=6,columnspan=3)
+delEvent_btn.grid(row=3,column=3,columnspan=3)
+
+
+
+
+editEvent_btn = Button(
+    button2_frame,
+    text='Edit Event',
+    font=('times 14'),
+    bg='gray',
+    fg='blue',
+    padx=20,
+    pady=10,
+    command=lambda:editEvent(day_dict,todo_this_week)
+)
+editEvent_btn.grid(row=3,column=6,columnspan=3)
+
+
+markEvent_btn = Button(
+    button2_frame,
+    text='Mark Completed',
+    font=('times 14'),
+    bg='gray',
+    fg = 'dark green',
+    padx=20,
+    pady=10,
+    command=lambda:markEventDone(day_dict,todo_this_week)
+)
+markEvent_btn.grid(row=4,column=0,columnspan=4)
+
+markEvent_btn = Button(
+    button2_frame,
+    text='Mark Not Yet Completed',
+    font=('times 14'),
+    bg='gray',
+    fg='red4',
+    padx=20,
+    pady=10,
+    command=lambda:markEventNotDone(day_dict,todo_this_week)
+)
+markEvent_btn.grid(row=4,column=5,columnspan=4)
+
+
+
 
 # --------------------------------------------- #
 
